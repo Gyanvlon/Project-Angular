1eb1cd84627ead245f1c3312a69f9586540802bc
gyan vlon <gyan.vlon212@gmail.com>
2018-12-15 16:41:35 +0530

    message

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  leaders:Leader[];
+selectLeader: Leader;
+
+  constructor(private leaderService: LeaderService) { }
+
+  ngOnInit() {
+    this.leaderService.getLeaders().subscribe(leaders => this.leaders = leaders);
+  }
+}
